01827501e92cb3a030862945a81bf5cee93299d8
katami-win <katami@win-ei.com>
2018-07-18 20:44:29 +0900

    Deleted redundant files.

---
+++ classes/tests/Accelerometer/build/CMakeFiles/AccelerometerTests.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/work/classes/tests/Accelerometer/AccelerometerTests.cpp" "/home/pi/work/classes/tests/Accelerometer/build/CMakeFiles/AccelerometerTests.dir/AccelerometerTests.cpp.o"
+  "/home/pi/work/classes/Accelerometer.cpp" "/home/pi/work/classes/tests/Accelerometer/build/CMakeFiles/AccelerometerTests.dir/home/pi/work/classes/Accelerometer.cpp.o"
+  "/home/pi/work/classes/Split.cpp" "/home/pi/work/classes/tests/Accelerometer/build/CMakeFiles/AccelerometerTests.dir/home/pi/work/classes/Split.cpp.o"
+  "/home/pi/work/classes/Task.cpp" "/home/pi/work/classes/tests/Accelerometer/build/CMakeFiles/AccelerometerTests.dir/home/pi/work/classes/Task.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
