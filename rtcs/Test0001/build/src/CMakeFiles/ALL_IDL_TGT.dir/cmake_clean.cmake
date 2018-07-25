5adae22062b3c92c527b71875008ff1dd6f5515f
katami-win <katami@win-ei.com>
2018-07-26 01:24:22 +0900

    Deleted redundant files.

--- rtcs/Test0001/build/src/CMakeFiles/ALL_IDL_TGT.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ALL_IDL_TGT"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/ALL_IDL_TGT.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
