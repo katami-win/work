64a03e1fc19c28338c7664238acdf27175bfec12
katami-win <katami@win-ei.com>
2018-07-29 18:56:13 +0900

    Delelted redundant files.

--- rtcs/backup/idlcompile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-omniidl -bpython -I"%RTM_ROOT%rtm\idl" -I"/usr/include/openrtm-1.1/rtm/idl/device_interfaces" -I"/usr/include/openrtm-1.1/rtm/idl" idl/BasicDataType.idl idl/BasicDataType.idl 
